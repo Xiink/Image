d61706f2074d7ec1a982900d161d2e8281a8f005
Xiink <wwr774926@gmail.com>
2019-05-06 17:08:07 +0800

    加入專案檔案。

---
+++ eVision-ShowImage/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace eVision_ShowImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.button_load = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LBL_PPM = new System.Windows.Forms.Label();
+            this.TXT_PX = new System.Windows.Forms.TextBox();
+            this.TXT_MM = new System.Windows.Forms.TextBox();
+            this.button_PPM = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Location = new System.Drawing.Point(10, 54);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(412, 309);
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(12, 16);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(108, 23);
+            this.button_load.TabIndex = 1;
+            this.button_load.Text = "Load color image";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(501, 24);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(108, 23);
+            this.btnGray.TabIndex = 2;
+            this.btnGray.Text = "Load gray image";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LBL_PPM);
+            this.groupBox1.Controls.Add(this.TXT_PX);
+            this.groupBox1.Controls.Add(this.TXT_MM);
+            this.groupBox1.Location = new System.Drawing.Point(126, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 36);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // LBL_PPM
+            // 
+            this.LBL_PPM.AutoSize = true;
+            this.LBL_PPM.Location = new System.Drawing.Point(222, 23);
+            this.LBL_PPM.Name = "LBL_PPM";
+            this.LBL_PPM.Size = new System.Drawing.Size(33, 12);
+            this.LBL_PPM.TabIndex = 2;
+            this.LBL_PPM.Text = "label1";
+            // 
+            // TXT_PX
+            // 
+            this.TXT_PX.Location = new System.Drawing.Point(105, 14);
+            this.TXT_PX.Name = "TXT_PX";
+            this.TXT_PX.Size = new System.Drawing.Size(100, 22);
+            this.TXT_PX.TabIndex = 1;
+            // 
+            // TXT_MM
+            // 
+            this.TXT_MM.Location = new System.Drawing.Point(22, 14);
+            this.TXT_MM.Name = "TXT_MM";
+            this.TXT_MM.Size = new System.Drawing.Size(76, 22);
+            this.TXT_MM.TabIndex = 0;
+            // 
+            // button_PPM
+            // 
+            this.button_PPM.Location = new System.Drawing.Point(408, 24);
+            this.button_PPM.Name = "button_PPM";
+            this.button_PPM.Size = new System.Drawing.Size(75, 23);
+            this.button_PPM.TabIndex = 4;
+            this.button_PPM.Text = "button1";
+            this.button_PPM.UseVisualStyleBackColor = true;
+            this.button_PPM.Click += new System.EventHandler(this.button_PPM_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(450, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "LoadModel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(450, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 54);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "OFFSET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(450, 231);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 54);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "MEASURE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(450, 291);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 54);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "CALCULATE";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 381);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_PPM);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.button_load);
+            this.Controls.Add(this.pbImage);
+            this.Name = "Form1";
+            this.Text = "eVision - Load Image and show";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label LBL_PPM;
+        private System.Windows.Forms.TextBox TXT_PX;
+        private System.Windows.Forms.TextBox TXT_MM;
+        private System.Windows.Forms.Button button_PPM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ eVision-ShowImage/Form1.cs
@@ -0,0 +1,302 @@
+﻿//eVision-Show Image Sample 
+//Function: 1. Load Image 
+//          2. convert Bitmap to EImageC24
+//          3. Show EImageC24 Image to picturebox
+//Enviornment: VS 2012, eVision 2.2.2
+//Caution: using eVision need to run [Visual Studio] or [compiled exe] as administrator, if not it will not able to get the grant from license dongle.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Euresys.Open_eVision_2_7;
+
+namespace eVision_ShowImage
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap = null;
+        BitmapData bmpData = null;
+        EImageC24 EC24Image1 = new EImageC24(); //eVision的彩色圖像物件
+        EImageBW8 EBW8Image1 = new EImageBW8(); //eVision的灰階圖像物件
+        float ScalingRatio = 0; //Picturebox與原始影像大小的縮放比例
+        float EBW8ScaleRatio;
+
+        EWorldShape EWorldShape1 = new EWorldShape();
+        EFrameShape EFrameShape1 = new EFrameShape();
+
+        EPatternFinder F1 = new EPatternFinder();
+        EFoundPattern[] F1FoundPatterns;
+
+        ECircleGauge C1 = new ECircleGauge();
+        ELineGauge L1 = new ELineGauge();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            //使用者選取Bitmap檔案
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //第一種載入作法:
+                ////讀取檔案的Bitmap (一般這一行也可以改成從相機的影像進行讀取）
+                //Bitmap bitmap_source = (Bitmap)Image.FromFile(openFileDialog1.FileName);
+                ////當bitmap尚未創建記憶體空間時直接使用Clone複製來源影像
+                //if (bitmap == null)
+                //    bitmap = (Bitmap)bitmap_source.Clone(); //複製完整的記憶體空間
+                ////指派影像
+                //bitmap = bitmap_source; 
+
+                ////防止來源影像為空則不處理
+                //if (bitmap == null)
+                //    return;
+                ////Bitmap影像轉換eVision格式影像
+                ////EC24Image1 = BitmapToEImageC24(ref bitmap);
+
+                //第二種載入作法:
+                EC24Image1.Load(openFileDialog1.FileName); //直接載入
+                
+                //顯示影像於Picturebox
+                ShowImage(EC24Image1, pbImage);
+
+                //因Bitmap容易造成記憶體外洩，因此將資料從記憶體中Dispose，並且進行GC
+                //bitmap.Dispose(); //第一種作法要記得取消記憶體的空間
+                System.GC.Collect();
+                System.GC.WaitForPendingFinalizers();
+            }
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            //使用者選取Bitmap檔案
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                EBW8Image1.Load(openFileDialog1.FileName); //直接載入
+
+                //顯示影像於Picturebox
+                ShowImage(EBW8Image1, pbImage);
+            }
+        }
+
+        /// <summary>
+        /// Bitmap影像轉換eVision格式影像
+        /// </summary>
+        /// <param name="bitmap">來源Bitmap影像</param>
+        /// <returns>轉換後的EImageC24影像</returns>
+        private EImageC24 BitmapToEImageC24(ref Bitmap bitmap)
+        {
+            EImageC24 EC24Image1 = null;
+            try
+            {
+                EC24Image1 = new EImageC24();
+
+                Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                bmpData =
+                    bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                    bitmap.PixelFormat);
+
+                EC24Image1.SetImagePtr(bitmap.Width, bitmap.Height, bmpData.Scan0);
+                bitmap.UnlockBits(bmpData);
+
+            }
+            catch (EException e)//EException為eVision的例外處理
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return EC24Image1;
+        }
+
+        /// <summary>
+        /// 顯示影像於Picturebox
+        /// </summary>
+        /// <param name="img">顯示的eVision影像</param>
+        /// <param name="pb">目標Picturebox</param>
+        private void ShowImage(EImageC24 img, PictureBox pb)
+        {
+            try
+            {
+                Bitmap bmp;
+                bmp = new Bitmap(pb.Width, pb.Height);
+
+                //計算Picturebox與顯示影像的比例，以便將影像縮放並且完整呈現到picturebox上。
+                float PictureBoxSizeRatio = (float)pb.Width / pb.Height;
+                float ImageSizeRatio = (float)img.Width / img.Height;
+                if (ImageSizeRatio > PictureBoxSizeRatio)
+                    ScalingRatio = (float)pb.Width / img.Width;
+                else
+                    ScalingRatio = (float)pb.Height / img.Height;
+
+                //委派
+                if (pb.InvokeRequired)
+                {
+                    pb.Invoke(new MethodInvoker(delegate() { img.Draw(Graphics.FromImage(bmp), ScalingRatio); pb.BackgroundImage = bmp; }));
+                }
+                else
+                {
+                    //先將EImageC24畫在bitmap模式的圖檔
+                    img.Draw(Graphics.FromImage(bmp), ScalingRatio);
+                    //再由Picturebox取得bitmap
+                    pb.BackgroundImage = bmp;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private void ShowImage(EImageBW8 img, PictureBox pb)
+        {
+            try
+            {
+                Bitmap bmp;
+                bmp = new Bitmap(pb.Width, pb.Height);
+
+                //計算Picturebox與顯示影像的比例，以便將影像縮放並且完整呈現到picturebox上。
+                float PictureBoxSizeRatio = (float)pb.Width / pb.Height;
+                float ImageSizeRatio = (float)img.Width / img.Height;
+                if (ImageSizeRatio > PictureBoxSizeRatio)
+                    ScalingRatio = (float)pb.Width / img.Width;
+                else
+                    ScalingRatio = (float)pb.Height / img.Height;
+                EBW8ScaleRatio = ScalingRatio; //記錄此比例供繪製輪廓軌跡使用
+                //委派
+                if (pb.InvokeRequired)
+                {
+                    pb.Invoke(new MethodInvoker(delegate () { img.Draw(Graphics.FromImage(bmp), ScalingRatio); pb.BackgroundImage = bmp; }));
+                }
+                else
+                {
+                    //先將EImageC24畫在bitmap模式的圖檔
+                    img.Draw(Graphics.FromImage(bmp), ScalingRatio);
+                    //再由Picturebox取得bitmap
+                    pb.BackgroundImage = bmp;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+
+            F1.AngleBias = 0f;
+            F1.AngleTolerance = 180.00f;
+            F1.ScaleTolerance = 0.05f;
+
+            this.C1.Attach(this.EFrameShape1);
+            this.C1.SetCenterXY(279f,325.5f);
+            this.C1.Tolerance = 20;
+            this.C1.Diameter = 104.69f;
+
+            this.L1.Attach(this.EFrameShape1);
+            this.L1.SetCenterXY(18.5f, 121.5f);
+            this.L1.Tolerance = 20;
+            this.L1.Length = 200;
+
+            this.EFrameShape1.Attach(EWorldShape1);
+            this.EFrameShape1.SetCenterXY(242f, 268.5f);
+        }
+
+        float ppm;
+        private void button_PPM_Click(object sender, EventArgs e)
+        {
+            ppm = float.Parse(this.TXT_MM.Text) / float.Parse(this.TXT_PX.Text);
+            this.LBL_PPM.Text = ppm.ToString("0.000");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = this.openFileDialog1.FileName;
+
+                this.F1.Load(fileName);
+
+                MessageBox.Show("OK");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.pbImage.CreateGraphics();
+
+            EFoundPattern firstMode1 = this.F1FoundPatterns[0];
+            firstMode1.DrawCenter = true;
+            firstMode1.DrawBoundingBox = true;
+            firstMode1.DrawFeaturePoints = true;
+            Color drawColor = Color.Red;
+            firstMode1.Draw(g, new ERGBColor(drawColor.R, drawColor.G, drawColor.B), ScalingRatio, ScalingRatio);
+
+            this.EFrameShape1.Draw(g, EDrawingMode.Nominal, false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EPoint center = this.F1FoundPatterns[0].Center;
+            float angle = this.F1FoundPatterns[0].Angle;
+
+            this.EFrameShape1.Angle = angle;
+            this.EFrameShape1.SetCenterXY(center.X, center.Y);
+
+            Graphics g = this.pbImage.CreateGraphics();
+            this.EFrameShape1.Draw(g, EDrawingMode.Nominal, false);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            C1.Measure(this.EBW8Image1);
+            L1.Measure(this.EBW8Image1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            float circleDiameter = C1.MeasuredCircle.Diameter * ppm;
+        }
+
+        /// <summary>計算兩點之間的長度</summary>
+        /// <param name="PointA">第一點座標</param>
+        /// <param name="PointB">第二點座標</param>
+        /// <param name="PointAToPointCLength">輸出: A 到 B 點距離</param>
+        /// <remarks>
+        /// </remarks>
+        /// <see>參考：http://163.32.100.89/koliming/volume1/1-2/1-2_ex-8/1-2_ex-8.html </see>
+        public static void CalLengthBetweenTwoPoint(PointF PointA, PointF PointB, out double PointAToPointCLength)
+        {
+            //取得AC線段長
+            PointAToPointCLength = Math.Sqrt(Math.Pow(PointA.X - PointB.X, 2) + Math.Pow(PointA.Y - PointB.Y, 2));
+        }
+
+        /// <summary>點到線垂直距離計算</summary>
+        /// <param name="lineStartPoint">線段起點</param>
+        /// <param name="lineEndPoint">線段終點</param>
+        /// <param name="measurePoint">量測點</param>
+        /// <param name="LineCrossPoint">回傳 點線距離計算垂直點</param>
+        /// <returns>點到線距離</returns>
+        public static double CalPointToLineVerticalPointDistance(PointF lineStartPoint, PointF lineEndPoint, PointF measurePoint, out PointF LineCrossPoint) //向量
+        {
+            PointF crossPos = new PointF();
+            double cross = (lineEndPoint.X - lineStartPoint.X) * (measurePoint.X - lineStartPoint.X) + (lineEndPoint.Y - lineStartPoint.Y) * (measurePoint.Y - lineStartPoint.Y);
+
+            double d2 = (lineEndPoint.X - lineStartPoint.X) * (lineEndPoint.X - lineStartPoint.X) + (lineEndPoint.Y - lineStartPoint.Y) * (lineEndPoint.Y - lineStartPoint.Y);
+            double r = cross / d2;
+            double px = lineStartPoint.X + (lineEndPoint.X - lineStartPoint.X) * r;
+            double py = lineStartPoint.Y + (lineEndPoint.Y - lineStartPoint.Y) * r;
+            crossPos.X = (float)px;
+            crossPos.Y = (float)py;
+            LineCrossPoint = crossPos;
+            return Math.Sqrt((measurePoint.X - px) * (measurePoint.X - px) + (py - measurePoint.Y) * (py - measurePoint.Y));
+        }
+    }
+}
